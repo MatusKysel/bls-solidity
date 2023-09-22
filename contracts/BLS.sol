6c1adbf062534254fecc966a275f1e59e827805a
Matus Kysel <MatusKysel@users.noreply.github.com>
2023-09-22 13:39:26 +0200

    Update BLS.sol

--- contracts/BLS.sol
+++ contracts/BLS.sol
@@ -1,6 +1,4 @@
-// SPDX-License-Identifier: MIT
-
-pragma solidity >=0.5.3 <0.7.0;
+pragma solidity >=0.6.12 <0.8.12;
 pragma experimental ABIEncoderV2;
 
 import "./BN256G1.sol";
@@ -20,22 +18,16 @@ contract BLS {
         uint256[2] y;
     }
 
-    /**
-     * @return The generator of G1.
-     */
-    function P1() internal pure returns (G1Point memory) {
+    function getG1Generator() internal pure returns (G1Point memory) {
         return G1Point(BN256G1.GX, BN256G1.GY);
     }
 
-    /**
-     * @return The generator of G2.
-     */
-    function P2() internal pure returns (G2Point memory) {
+    function getG2Generator() internal pure returns (G2Point memory) {
         return
-            G2Point({
-                x: [BN256G2.G2_NEG_X_RE, BN256G2.G2_NEG_X_IM],
-                y: [BN256G2.G2_NEG_Y_RE, BN256G2.G2_NEG_Y_IM]
-            });
+            G2Point(
+                [BN256G2.G2_NEG_X_RE, BN256G2.G2_NEG_X_IM],
+                [BN256G2.G2_NEG_Y_RE, BN256G2.G2_NEG_Y_IM]
+            );
     }
 
     function hashToG1(bytes calldata _msg)
@@ -47,81 +39,38 @@ contract BLS {
         return G1Point(x, y);
     }
 
+    function verifyPairing(G1Point memory point1, G2Point memory point2)
+        internal
+        returns (bool)
+    {
+        G1Point memory g1Generator = getG1Generator();
+        G2Point memory g2Generator = getG2Generator();
+        return
+            BN256G1.bn256CheckPairing(
+                [
+                    point1.x,
+                    point1.y,
+                    point2.x[0],
+                    point2.x[1],
+                    point2.y[0],
+                    point2.y[1],
+                    g1Generator.x,
+                    g1Generator.y,
+                    g2Generator.x[0],
+                    g2Generator.x[1],
+                    g2Generator.y[0],
+                    g2Generator.y[1]
+                ]
+            );
+    }
+
     function verifySignature(
         G2Point calldata publicKey,
         bytes calldata _message,
         G1Point calldata signature
     ) public returns (bool) {
-        G1Point memory msgHash = hashToG1(_message);
-        G2Point memory g2 = P2();
-        bool pairingValid = BN256G1.bn256CheckPairing(
-            [
-                msgHash.x,
-                msgHash.y,
-                publicKey.x[0],
-                publicKey.x[1],
-                publicKey.y[0],
-                publicKey.y[1],
-                signature.x,
-                signature.y,
-                g2.x[0],
-                g2.x[1],
-                g2.y[0],
-                g2.y[1]
-            ]
-        );
-        return pairingValid;
-    }
-
-    function aggregatePublicKey(G2Point[] memory publicKeys)
-        public
-        returns (G2Point memory)
-    {
-        G2Point memory aggpk;
-        for (uint256 i = 0; i < publicKeys.length; i++) {
-            (uint256 xx, uint256 xy, uint256 yx, uint256 yy) = BN256G2
-                .ecTwistAdd(
-                    aggpk.x[0],
-                    aggpk.x[1],
-                    aggpk.y[0],
-                    aggpk.y[1],
-                    publicKeys[i].x[0],
-                    publicKeys[i].x[1],
-                    publicKeys[i].y[0],
-                    publicKeys[i].y[1]
-                );
-            aggpk.x[0] = xx;
-            aggpk.x[1] = xy;
-            aggpk.y[0] = yx;
-            aggpk.y[1] = yy;
-        }
-        return aggpk;
-    }
-
-    function verifyAggregatedSignature(
-        G2Point[] calldata publicKeys,
-        bytes calldata _message,
-        G1Point calldata signature
-    ) public returns (bool) {
-        G2Point memory publicKey = aggregatePublicKey(publicKeys);
-        G1Point memory msgHash = hashToG1(_message);
-        G2Point memory g2 = P2();
-        bool pairingValid = BN256G1.bn256CheckPairing(
-            [
-                msgHash.x,
-                msgHash.y,
-                publicKey.x[0],
-                publicKey.x[1],
-                publicKey.y[0],
-                publicKey.y[1],
-                signature.x,
-                signature.y,
-                g2.x[0],
-                g2.x[1],
-                g2.y[0],
-                g2.y[1]
-            ]
-        );
-        return pairingValid;
+        return
+            verifyPairing(hashToG1(_message), publicKey) &&
+            verifyPairing(signature, getG2Generator());
     }
 }
